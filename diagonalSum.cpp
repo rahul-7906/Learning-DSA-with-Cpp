a74ea69e4ac9ead9ed65d47d906a9ff90aae2a80
Rahul <rahulmelkani4281@gmail.com>
2026-08-16 09:35:43 +0530

    Update solutions for  diagonal sum

--- diagonalSum.cpp
+++ diagonalSum.cpp
@@ -23,7 +23,7 @@ int main(){
                          {5,6,7,8},
                          {9,10,11,12},
                          {13,14,15,16}};
-    
+     
     diagonalSum(matrix,4,4);
 
     return 0;
