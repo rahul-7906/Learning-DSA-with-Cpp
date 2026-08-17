457592f755c398953312850a7f980e83511fdb2e
rahul-7906 <rahulmelkani4281@gmail.com>
2026-08-17 05:53:00 +0530

    learning more anout character arrays

---
+++ char.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){ 
+
+    char str[5] = { 'c', 'o' ,'d' ,'e' ,'\0'};
+    cout << str << endl;
+    cout << strlen(str);
+
+    // char word[30];
+    // cin >> word;
+    // cout << word;
+
+    // char word[30];
+    // cin.getline(word,30);
+    // cout << word;
+   
+
+    return 0;
+}
