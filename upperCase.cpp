9d8232feca7cb5ee2df562728f8084126493d7e3
rahul-7906 <rahulmelkani4281@gmail.com>
2026-08-17 06:22:01 +0530

    Add toUpperCase function to convert string case
    Implement a function to convert a string to uppercase.

---
+++ upperCase.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void toUpperCase(char str[],int n){
+    for(int i = 0;i<n;i++){
+        char ch = str[i];
+        if(ch>='A' && ch<= 'Z'){
+            continue;
+        } else {
+            str[i] = ch - 'a' + 'A';
+        }
+    }
+}
+
+
+int main(){
+     char str[30] = "RaHul";
+     toUpperCase(str,strlen(str));
+     cout << str << endl;
+    return 0;
+}
