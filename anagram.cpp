ef41aa966ae5cc59215add267b733cfb7a3848fb
rahul-7906 <rahulmelkani4281@gmail.com>
2026-08-18 14:47:10 +0530

    learnig the anagram

---
+++ anagram.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isAnagram(string str1,string str2){
+    if(str1.length() != str2.length()){
+        cout << "Not avalid anagram";
+        return false;
+    }
+
+    int count[26] = {0};
+    for(int i = 0;i< str1.length();i++){
+        int idx = str1[i]-'a';
+        count[idx]++;
+    }
+
+    for(int i = 0; i < str2.length(); i++){
+        int idx = str2[i]-'a';
+        if(count[idx]==0){
+            cout << "Not avalid anagram";
+            return false;
+        }
+        count[idx]--;
+    }
+    cout<< "valid anagram";
+    return true;
+}
+
+
+int main(){
+    string str1 = "anagram";
+    string str2 = "nagaram";
+    
+    isAnagram(str1,str2);
+
+    return 0;
+}
